2497158a22e52d9a1e018441111682ce7856e6c2
suryakant thombare <suryakant.thombare@ispace.com>
2023-03-13 22:20:15 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+let name: string = "Surya";
+let age: number = 22
+
+function App() {
+  return (
+    <div className="App">
+      Hello `{name}({age})
+    </div>
+  );
+}
+
+export default App;
