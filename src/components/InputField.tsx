d9267bdfb4f40f9209c8fe2f147df24a5672606b
suryakant thombare <suryakant.thombare@ispace.com>
2023-03-15 00:48:17 +0530

    Drag and Drop implimented

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,39 +1,35 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
-interface Props {
+interface props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
-  handleAdd:(e:React.FormEvent)=>void;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
-
-    const inputRef = useRef<HTMLInputElement>(null)
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <div>
-      <form className="input" onSubmit={(e)=>{
-      handleAdd(e)
-      inputRef.current?.blur()
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
       }}
-      
-      >
-        <input 
-        type="input" 
-        ref={inputRef}
-        placeholder="Enter a Task" 
-        onChange={(e) => {
-            setTodo(e.target.value)
-        }}
-        className="input__box" 
+    >
+      <input
+        type="text"
+        placeholder="Enter a Task"
         value={todo}
-        />
-        <button type="submit" className="input_submit">
-          Go{" "}
-        </button>
-      </form>
-    </div>
+        ref={inputRef}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
+      <button type="submit" className="input_submit">
+        GO
+      </button>
+    </form>
   );
 };
 
